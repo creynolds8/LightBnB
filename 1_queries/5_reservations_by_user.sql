137fa7630375dd5e98103135b77c6af300106e19
creynolds8 <cameron8.cr@gmail.com>
2024-05-29 18:09:07 -0700

    update query to accurately get expected results, error in joining logic w/ property reviews table

--- 1_queries/5_reservations_by_user.sql
+++ 1_queries/5_reservations_by_user.sql
@@ -1,6 +1,8 @@
-SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night
+SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, AVG(rating)
 FROM reservations
 JOIN properties ON properties.id = property_id
-WHERE guest_id = 1
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1 
+GROUP BY reservations.id, properties.title, properties.cost_per_night
 ORDER BY start_date
 LIMIT 10;
